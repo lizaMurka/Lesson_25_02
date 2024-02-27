d8cfa8e19097e3262e973911011569916a6547c3
Sveta <sveta301976@gmail.com>
2024-02-27 10:29:15 +0200

    the first task is completed without 3

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+numbers = [1, 2, 3, 4, 1, 2, 5, 6, 7, 3, 8]
+unique_numbers = set(numbers)
+print(unique_numbers)
 
+numbers_1 = [1, 5, 2, 9, 1, 6, 9]
+numbers_2 = [3, 7, 2, 4, 6, 3, 8]
+set1 = set(numbers_1)
+set2 = set(numbers_2)
+intersection = set1 & set2
+count_unique_numbers = len(intersection)
+print(f"Number of unique numbers in common: {count_unique_numbers}")
+
+text = "The first line contains the number of rows, followed by the rows themselves."
