39c135e460459efb6d8e5595061fe4fc7d00e86e
Sveta <sveta301976@gmail.com>
2024-02-27 10:47:01 +0200

    other task completed

---
+++ dictionaries.py
@@ -0,0 +1,26 @@
+countries_cities = {
+
+"Ukraine": ["Kyiv", "Lviv", "Dnipro"],
+
+"USA": ["Los Angeles", "Las Vegas"],
+
+"Germany": ["Berlin", "Hamburg", " Munich"],
+
+"Italy": ["Rome", "Milan", "Naples"],
+
+"Japan": ["Nagoya", "Toyohashi", "Okazaki"],
+
+"Australia": ["Sydney", "Melbourne", "Brisbane"]
+}
+print(countries_cities)
+
+
+
+keys = ["key1", "key2", "key3"]
+values = ["value1", "value2", "value3"]
+
+my_dict = {}
+for i in range(len(keys)):
+  my_dict[keys[i]] = values[i]
+
+print(my_dict)
